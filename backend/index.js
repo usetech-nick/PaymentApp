da4487789aa4331bda15fa08cc36a184a38ec48d
Nick <nkg9033@gmail.com>
2025-07-31 19:01:15 +0000

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+const app = express();
+const rootRouter = require("./routes/index");
 
+app.use(express.json());
+app.use(cors());
+app.use("/api/v1", rootRouter);
 
+app.listen(3000, () => {
+  console.log("Sever running at port 3000");
+});
---
+++ backend/routes/user.js
@@ -0,0 +1,126 @@
+const express = require("express");
+const router = express.Router();
+const { JWT_SECRET } = require("../config");
+const zod = require("zod");
+const jwt = require("jsonwebtoken");
+const User = require("../db");
+const { authMiddleware } = require("../middleware");
+
+//signup
+const signupBody = zod.object({
+  username: zod.string().email(),
+  firstName: zod.string(),
+  lastName: zod.string(),
+  password: zod.string(),
+});
+
+router.post("/signup", async (req, res) => {
+  const { success } = signupBody.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "Email already taken/incorrect inputs",
+    });
+  }
+
+  const existingUser = await User.findOne({
+    username: req.body.username,
+  });
+  if (existingUser) {
+    return res.status(411).json({
+      message: "Email already taken/incorrect inputs",
+    });
+  }
+
+  const user = await User.create({
+    username: req.body.username,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    password: req.body.password,
+  });
+  const userId = user._id;
+
+  const token = jwt.sign({ userId }, JWT_SECRET);
+
+  res.json({
+    message: "User created successfully !",
+    token: token,
+  });
+});
+
+//signin
+const signinBody = zod.object({
+  username: zod.string().email(),
+  password: zod.string(),
+});
+router.post("/signin", async (req, res) => {
+  const { success } = signinBody.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "Email already taken/incorrect inputs",
+    });
+  }
+  const user = await User.findOne({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  if (user) {
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET);
+    res.json({
+      message: "Login successful!",
+      token: token,
+    });
+    return;
+  }
+  return res.status(411).json({
+    message: "Login failed",
+  });
+});
+
+//updateInfo
+const updateBody = zod.object({
+  firstName: zod.string(),
+  lastName: zod.string(),
+  password: zod.string(),
+});
+router.put("/", authMiddleware, async (req, res) => {
+  const { success } = updateBody.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "Error while updating information",
+    });
+  }
+  await User.updateOne({ _id: req.userId }, req.body);
+  res.json({
+    message: "Updated successfully",
+  });
+});
+
+//search
+router.get("/bulk", async (req, res) => {
+  const filter = req.query.filter || "";
+
+  const users = await User.find({
+    $or: [
+      {
+        firstName: {
+          $regex: filter,
+        },
+      },
+      {
+        lastName: {
+          $regex: filter,
+        },
+      },
+    ],
+  });
+  res.json({
+    user: users.map((user) => ({
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
+  });
+});
+
+module.exports = router;
